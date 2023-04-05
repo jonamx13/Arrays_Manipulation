cd791ddb4184f8ec2bc3dfd7b73aeed8df9a5ff5
Jonathan Meixueiro <lav.jonathanom@gmail.com>
2023-04-04 17:32:22 -0700

    flatmap, TODO: class challenge

---
+++ server/15-flatmap.js
@@ -0,0 +1,39 @@
+const users = [
+    { userId: 1, username: 'Tom', attributes: ['Nice', 'Cute'] },
+    { userId: 2, username: 'Mike', attributes: ['Lovely'] },
+    { userId: 3, username: 'Nico', attributes: ['Nice', 'Cool'] },
+];
+
+const rta = users.map( user => user.attributes ).flat();
+const rta2 = users.flatMap(user => user.attributes);
+
+console.log('map-flat', rta);
+console.log('flatmap', rta2);
+
+//*Class challenge
+const calendars = {
+    primaryCalendar: [
+      {
+        startDate: new Date(2021, 1, 1, 15),
+        endDate: new Date(2021, 1, 1, 15, 30),
+        title: "Cita 1",
+      },
+      {
+        startDate: new Date(2021, 1, 1, 17),
+        endDate: new Date(2021, 1, 1, 18),
+        title: "Cita 2",
+      },
+    ],
+    secondaryCalendar: [
+      {
+        startDate: new Date(2021, 1, 1, 12),
+        endDate: new Date(2021, 1, 1, 12, 30),
+        title: "Cita 2",
+      },
+      {
+        startDate: new Date(2021, 1, 1, 9),
+        endDate: new Date(2021, 1, 1, 10),
+        title: "Cita 4",
+      },
+    ],
+  };
